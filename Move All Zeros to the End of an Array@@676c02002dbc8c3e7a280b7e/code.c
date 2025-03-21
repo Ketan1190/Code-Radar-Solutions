5428029274805c4e7672f9a612b9922599778113
Ketan1190 <ketan1190.becse24@chitkara.edu.in>
2025-03-22 00:38:19 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-void swap(int arr[],int a,int b){
-    int temp=arr[a];
-    arr[a]=arr[b];
-    arr[b]=temp;
+void swap(int arr[], int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
 
 int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
 
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    // Reading input
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    int end=n-1;
-    for(int j=0;j<end;j++){
-        if(arr[j]==0){
-            swap(arr,j,end);
+    int end = n - 1;
+    for (int j = 0; j <= end; j++) {
+        while (arr[j] == 0 && j < end) { // Ensure the swapped element is checked
+            swap(arr, j, end);
             end--;
         }
     }
-    for(int k=0;k<n;k++){
-        printf("%d ",arr[k]);
+
+    // Printing the array
+    for (int k = 0; k < n; k++) {
+        printf("%d ", arr[k]);
     }
+
+    return 0;
 }
